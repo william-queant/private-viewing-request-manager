f86f215241a64c9e5976729a6af0088c19d8c22b
william-queant <william.queant@gmail.com>
2025-06-01 12:55:16 +1200

    Git init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
